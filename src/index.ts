fe7ab44dab20446bddc006b4d338cab11942ac16
ryo-jojo@docomo.ne.jp <59068345+saito263@users.noreply.github.com>
2021-10-27 04:37:04 +0900

    eslintとprettierを追加

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,25 @@ const CANVAS_WIDTH = 960;
 const CANVAS_HEIGHT = 540;
 
 const config: Phaser.Types.Core.GameConfig = {
-  title: 'マイゲーム',
-  type: Phaser.AUTO,
-  backgroundColor: '#48C4F8',
-  pixelArt: true,
-  scale: {
-    parent: 'game',
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    mode: Phaser.Scale.FIT,
-    width: CANVAS_WIDTH,
-    height: CANVAS_HEIGHT
-  },
-  scene: Scenes,
-  physics: {
-  	default: 'matter',
-  	matter: {
-  	  gravity: { y: 0 },
-      debug: false
-  	}
-  }
+    title: 'マイゲーム',
+    type: Phaser.AUTO,
+    backgroundColor: '#48C4F8',
+    pixelArt: true,
+    scale: {
+        parent: 'game',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        mode: Phaser.Scale.FIT,
+        width: CANVAS_WIDTH,
+        height: CANVAS_HEIGHT,
+    },
+    scene: Scenes,
+    physics: {
+        default: 'matter',
+        matter: {
+            gravity: { y: 0 },
+            debug: false,
+        },
+    },
 };
 
 export const game: Phaser.Game = new Phaser.Game(config);
